ac2cd35ceeda6dffa4f2e252a3827b9801967495
auliadj <auliadwijmrn@gmail.com>
2025-04-16 16:35:25 +0700

    Update home.blade.php
    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ini halaman home</title>
+    <title>Home blade</title>
 </head>
 <body>
-    
+    <h1>Ini Halaman Home</h1>
 </body>
 </html>
